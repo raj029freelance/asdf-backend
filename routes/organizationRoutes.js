7c0fdf86c5687e80f8c480106757d9510e160df0
raj029freelance <90445876+raj029freelance@users.noreply.github.com>
2022-08-21 23:50:59 +0530

    Add files via upload

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const organizationController = require("../controllers/organizationController");
 
-router.route("/").get(organizationController.getAllOrganization).post(organizationController.createOrganization)
-router.route("/paginated/all").get(organizationController.getPaginatedOrganization)
+router
+  .route("/")
+  .get(organizationController.getAllOrganization)
+  .post(organizationController.createOrganization);
+router
+  .route("/paginated/all")
+  .get(organizationController.getPaginatedOrganization);
 router.route("/all").post(organizationController.insertAllOrganizations);
-router.route("/query").get(organizationController.getAllQueries).post(organizationController.postQuery);
+router
+  .route("/query")
+  .get(organizationController.getAllQueries)
+  .post(organizationController.postQuery);
 router.route("/query/:id").delete(organizationController.deleteQuery);
-router.route("/:id").get(organizationController.getOrganization).patch(organizationController.updateOrganization).delete(organizationController.deleteOrganization);
+router
+  .route("/:id")
+  .get(organizationController.getOrganization)
+  .post(organizationController.updateOrganization)
+  .delete(organizationController.deleteOrganization);
 module.exports = router;
