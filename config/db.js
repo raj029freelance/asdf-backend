150174b26af0b5e07a36bc5c1643fead4d6f600c
Amsavarthan Lv <80940598+amsavarthan@users.noreply.github.com>
2022-08-22 12:13:50 +0530

    Added mongo uri as env variable

--- config/db.js
+++ config/db.js
@@ -4,15 +4,12 @@ const mongoose = require("mongoose");
 
 const InitiateMongoServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://saran1234:saran1234@cluster0.rxropif.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to DB !!");
   } catch (e) {
     console.log(e);
