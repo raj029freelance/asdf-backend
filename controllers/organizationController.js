5bb76f4fc6877c9eb07f64862e4f400baa658e11
Amsavarthan Lv <80940598+amsavarthan@users.noreply.github.com>
2022-10-16 01:14:41 +0530

    Removed loop

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -54,11 +54,12 @@ const getExactSearch = async(CompanyName) => {
 
         const regexExpression = new RegExp(["^", CompanyName, "$"].join(""), "i");
         const regexArr = [regexExpression]
-        CompanyName.split(" ").forEach((word)=>{
-            if(word.length<3)return;
-            const regex=new RegExp(["^", word, "$"].join(""), "i");
-            regexArr.push(regex)
-        })
+        
+        // CompanyName.split(" ").forEach((word)=>{
+        //     if(word.length<3)return;
+        //     const regex=new RegExp(["^", word, "$"].join(""), "i");
+        //     regexArr.push(regex)
+        // })
 
         const searchResults = await Organization.find({
             $or:regexArr
